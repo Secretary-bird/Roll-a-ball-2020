c4db610af989e8ddc12054aa8b70860c405f71e3
Secretary-bird <LLopez31098@gmail.com>
2020-01-31 20:22:10 -0500

    working on it

--- Assets/New Scripts/BallControler.cs
+++ Assets/New Scripts/BallControler.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
 public class BallControler : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+
     private Rigidbody rb;
+    private int count;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -18,5 +24,19 @@ public class BallControler : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Ups"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + count.ToString();
     }
 }
